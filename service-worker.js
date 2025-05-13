4890f3052bafcf03ac105bd9d95b2af224052f8c
ratskull24 <66019758+ratskull24@users.noreply.github.com>
2025-05-13 15:46:09 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -30,3 +30,25 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/main.js', // adjust path if it's in js/
+                '/assets/icons/icon-192x192.png',
+                '/assets/icons/icon-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
