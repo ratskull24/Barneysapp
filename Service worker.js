8ee94d6cba893cf52d2597c348e3df6867eec3c5
ratskull24 <66019758+ratskull24@users.noreply.github.com>
2025-05-13 13:09:03 -0400

    Create Service worker.js

---
+++ Service worker.js
@@ -0,0 +1,32 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch((error) => {
+            console.log('Service Worker registration failed:', error);
+        });
+    });
+}
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/js/main.js',
+                '/assets/icons/icon-192x192.png',
+                '/assets/icons/icon-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
